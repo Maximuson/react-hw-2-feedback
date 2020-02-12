559cceb1beea87ba17aadd351f90dc9fb7d6ca11
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-02-12 17:19:49 +0200

    refactoring to func components

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Component } from 'react';
 
-class FeedbackOptions extends Component {
-  render() {
-    const { chooseGood, chooseNeutral, chooseBad } = this.props;
-    return (
-      <div>
-        <button type="button" onClick={chooseGood}>
-          Good
-        </button>
-        <button type="button" onClick={chooseNeutral}>
-          Neutral
-        </button>
-        <button type="button" onClick={chooseBad}>
-          Bad
-        </button>
-      </div>
-    );
-  }
-}
+const FeedbackOptions = props => {
+  const { chooseGood, chooseNeutral, chooseBad } = props;
+
+  return (
+    <div>
+      <button type="button" onClick={chooseGood}>
+        Good
+      </button>
+      <button type="button" onClick={chooseNeutral}>
+        Neutral
+      </button>
+      <button type="button" onClick={chooseBad}>
+        Bad
+      </button>
+    </div>
+  );
+};
 
 export default FeedbackOptions;
